3c68d682cf4708015448216bbeb661b2ac6ecd4a
Hyzer <98345064+Hyzerr@users.noreply.github.com>
2022-02-25 15:46:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,10 +16,10 @@ gc1 = ''
 gc2 = ''
 gc3 = ''
 global.linkGC = ['https://chat.whatsapp.com/HAZ6yFgCafUAeDbNH33IrL', ''] // ganti jadi group lu
-global.owner = ['6288279268363'] // Put your number here //owner eval
-global.kontak = ['6288279268363','0'] //Ketika ada yang ngetik #owner
-global.mods = ['6288279268363'] // Want some help?
-global.prems = ['6288279268363'] // Premium user has unlimited limit
+global.owner = ['6282361160044'] // Put your number here //owner eval
+global.kontak = ['6282361160044','0'] //Ketika ada yang ngetik #owner
+global.mods = ['6282361160054'] // Want some help?
+global.prems = ['6282361160044'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
@@ -54,23 +54,23 @@ leyskey = 'dappakntlll'
 bearkey = 'buy'
 
 //xteam        MIMINETBOT
-namaig = 'https://instagram.com/drak_ipul123'
-namagithub = 'GAK ADA :)'
+namaig = 'https://instagram.com/shiroo_craziiphat'
+namagithub = 'https://github.com/Hyzerr'
 kasihcaption = `Nih kak`
-namakontak1 = 'King Of Bear'
-namakontak2 = 'King Of Bear'
+namakontak1 = 'Hyzer'
+namakontak2 = 'Hyzerr'
 
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
-global.packname = 'By King Of Bear' // ganti aja
-global.author = 'King Of Bear' // ganti aja
+global.packname = 'Shiro Botz' // ganti aja
+global.author = '<(￣︶￣)↗' // ganti aja
 
 //yyy
-bc = 'King Of Bear' //King Of Bear Broadcast
-footer = '\n©King Of Bear'
-namabot = 'YUKI BOT'
-namalu = 'King Of Bear'
+bc = 'Hyzer' //Shiro Botz Broadcast
+footer = '\n©Hyzer.'
+namabot = 'Shiro Botz'
+namalu = 'Hyzer'
 
 
 // 
